d41be45b06cf016f010949d25b9769b998b4406d
yana <yarmenova@gmail.com>
2023-12-10 21:07:15 +0000

    Added basic form handling and typewriter plug-in

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateGame(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "Musical chairs",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let gameFormElement = document.querySelector("#game-generator-form");
+gameFormElement.addEventListener("submit", generateGame);
