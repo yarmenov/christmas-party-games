0a0f8856ff613be4a22bc1a9781f9b202a3454a2
yana <yarmenova@gmail.com>
2023-12-17 17:35:34 +0000

    updated the context for the AI in js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ function generateGame(event) {
   let apiKey = "3ea9t1312bbo2ff30fa1b499f43ab01f";
   let prompt = `User instructions: Generate a Christmas party game about${instructionsInput.value} `;
   let context =
-    "You love Christmas and you know how to throw a good party. You know a lot of funny Christmas party games on different topics.Please generate a Christmas party game idea following the user instructions. Please show a maximum of 5 lines and present the result in bullet points please. Show each bullet point on a new line.";
+    "You love Christmas and you know how to throw a good party. You know a lot of funny Christmas party games on different topics.Please generate a Christmas party game idea following the user instructions. Please present the result in bullet points please. Show each bullet point on a new line.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let poemElement = document.querySelector("#game");
