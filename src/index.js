f6bc1671fb5d413ba75f8230888498c33b6a2714
yana <yarmenova@gmail.com>
2023-12-17 17:33:15 +0000

    improved the user experience

--- src/index.js
+++ src/index.js
@@ -13,9 +13,12 @@ function generateGame(event) {
   let apiKey = "3ea9t1312bbo2ff30fa1b499f43ab01f";
   let prompt = `User instructions: Generate a Christmas party game about${instructionsInput.value} `;
   let context =
-    "You love Christmas and you know how to throw a good party. You are a comedian and you know a lot of funny Christmas party games on different topics.Please generate a Christmas party game idea following the user instructions. Use new lines for the description. ";
+    "You love Christmas and you know how to throw a good party. You know a lot of funny Christmas party games on different topics.Please generate a Christmas party game idea following the user instructions. Please show a maximum of 5 lines and present the result in bullet points please. Show each bullet point on a new line.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let poemElement = document.querySelector("#game");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a Christmas party game about ${instructionsInput.value}</div>`;
   axios.get(apiUrl).then(displayGame);
 }
 
